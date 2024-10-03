0d41aff4e9c8a0fa73be531be0e8eac30cc30627
Anasch113 <anaschaudry2002@gmail.com>
2024-10-04 00:14:18 +0500

    integration setup for llm

--- integration.definition.ts
+++ integration.definition.ts
@@ -1,9 +1,21 @@
-import { IntegrationDefinition } from '@botpress/sdk'
+import { IntegrationDefinition, interfaces, z } from '@botpress/sdk'
 import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
-  name: integrationName,
+  name: "llama-3",
+  entities: {
+    modelRef: {
+      schema: z.object({
+        id: z.string(),
+      })
+    },
+  },
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
 })
+  .extend(interfaces.llm, ({ modelRef }) => ({
+    modelRef
+  }))
+
+  
